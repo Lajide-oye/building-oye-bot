a77d6f8afbbf86f48a9c6dc44cf4a251ab437c55
Lajide-oye <georgepatrickolajide@gmail.com>
2025-07-13 00:38:53 +0100

    Twitter update

--- twitter.py
+++ twitter.py
@@ -1,15 +1,4 @@
-import tweepy
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def tweet(message):
-    auth = tweepy.OAuth1UserHandler(
-        os.getenv("CONSUMER_KEY"),
-        os.getenv("CONSUMER_SECRET"),
-        os.getenv("ACCESS_TOKEN"),
-        os.getenv("ACCESS_TOKEN_SECRET"),
-    )
-    api = tweepy.API(auth)
-    api.update_status(message)
+    print("🚀 This is what would be tweeted:")
+    print(message)
